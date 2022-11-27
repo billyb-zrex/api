3414120e63dd6b61ca27f5676c0ac113d2e2df3e
akash <akash@sharefable.com>
2022-11-27 13:43:46 +0000

    Support for adding user

--- src/main/java/com/sharefable/api/controller/v1/WorkspaceController.java
+++ src/main/java/com/sharefable/api/controller/v1/WorkspaceController.java
@@ -3,8 +3,10 @@ package com.sharefable.api.controller.v1;
 import com.sharefable.api.common.ApiResp;
 import com.sharefable.api.controller.Routes;
 import com.sharefable.api.service.WorkspaceService;
-import com.sharefable.api.transport.NewOrgReqBody;
-import com.sharefable.api.transport.NewOrgResp;
+import com.sharefable.api.transport.NewOrgReq;
+import com.sharefable.api.transport.NewUserReq;
+import com.sharefable.api.transport.OrgResp;
+import com.sharefable.api.transport.UserResp;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +18,16 @@ import java.util.Optional;
 @RestController
 @RequestMapping(Routes.API_V1)
 @Slf4j
-public class OrgController {
+public class WorkspaceController {
     private final WorkspaceService wsService;
 
     @Autowired
-    public OrgController(WorkspaceService wsService) {
+    public WorkspaceController(WorkspaceService wsService) {
         this.wsService = wsService;
     }
 
     @RequestMapping(value = Routes.NEW_ORG, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ApiResp createNewProject(@RequestBody NewOrgReqBody body) {
+    public ApiResp createNewProject(@RequestBody NewOrgReq body) {
         String name = body.getDisplayName();
         if (StringUtils.isBlank(name)) {
             log.error("Org name should not be blank");
@@ -33,7 +35,7 @@ public class OrgController {
                 .errStr("Org name should not be blank").build();
         }
         body.setDisplayName(body.getDisplayName().trim());
-        NewOrgResp org = wsService.newOrg(body);
+        OrgResp org = wsService.newOrg(body);
         return ApiResp.builder().status(ApiResp.ResponseStatus.Success).data(org).build();
     }
 
@@ -43,7 +45,13 @@ public class OrgController {
             return ApiResp.builder().status(ApiResp.ResponseStatus.Failure).errCode(ApiResp.ErrorCode.IllegalArgs)
                 .errStr("Missing parameter").build();
         }
-        NewOrgResp org = wsService.getOrgById(id.get());
+        OrgResp org = wsService.getOrgById(id.get());
         return ApiResp.builder().status(ApiResp.ResponseStatus.Success).data(org).build();
+    }
+
+    @RequestMapping(value = Routes.NEW_USER, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResp newUser(@RequestBody NewUserReq body) {
+        UserResp user = wsService.newUser(body);
+        return ApiResp.builder().status(ApiResp.ResponseStatus.Success).data(user).build();
     }
 }
--- src/main/java/com/sharefable/api/service/WorkspaceService.java
+++ src/main/java/com/sharefable/api/service/WorkspaceService.java
@@ -3,9 +3,13 @@ package com.sharefable.api.service;
 import com.sharefable.api.common.ImageType;
 import com.sharefable.api.common.Utils;
 import com.sharefable.api.entity.Org;
+import com.sharefable.api.entity.User;
 import com.sharefable.api.repo.OrgRepo;
-import com.sharefable.api.transport.NewOrgReqBody;
-import com.sharefable.api.transport.NewOrgResp;
+import com.sharefable.api.repo.UserRepo;
+import com.sharefable.api.transport.NewOrgReq;
+import com.sharefable.api.transport.NewUserReq;
+import com.sharefable.api.transport.OrgResp;
+import com.sharefable.api.transport.UserResp;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.javatuples.Pair;
@@ -21,15 +25,17 @@ import java.util.UUID;
 public class WorkspaceService {
     private final OrgRepo orgRepo;
     private final S3Service s3Service;
+    private final UserRepo userRepo;
 
     @Autowired
-    public WorkspaceService(OrgRepo orgRepo, S3Service s3Service) {
+    public WorkspaceService(OrgRepo orgRepo, UserRepo userRepo, S3Service s3Service) {
         this.orgRepo = orgRepo;
         this.s3Service = s3Service;
+        this.userRepo = userRepo;
     }
 
     @Transactional
-    public NewOrgResp newOrg(NewOrgReqBody body) {
+    public OrgResp newOrg(NewOrgReq body) {
         String displayName = body.getDisplayName();
         String rid = Utils.createReadableId(displayName);
 
@@ -47,13 +53,27 @@ public class WorkspaceService {
         }
         Org org = orgBuilder.build();
         Org savedOrg = orgRepo.save(org);
-        return NewOrgResp.from(savedOrg);
+        return OrgResp.from(savedOrg);
     }
 
     @Transactional(readOnly = true)
-    public NewOrgResp getOrgById(Long id) {
+    public OrgResp getOrgById(Long id) {
         // TODO check if user has access to get the org
         Optional<Org> org = orgRepo.findById(id);
-        return org.map(NewOrgResp::from).orElse(NewOrgResp.Empty());
+        return org.map(OrgResp::from).orElse(OrgResp.Empty());
+    }
+
+    @Transactional
+    public UserResp newUser(NewUserReq body) {
+        User user = User.builder()
+            .firstName(body.getFirstName())
+            .lastName(body.getLastName())
+            .email(body.getEmail())
+            .avatar(body.getAvatar())
+            .belongsToOrg(Org.builder().id(body.getBelongsToOrg()).build())
+            .build();
+
+        User savedUser = userRepo.save(user);
+        return UserResp.from(savedUser);
     }
 }
