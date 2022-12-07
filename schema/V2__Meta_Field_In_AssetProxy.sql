ba89915602370e090247b622d1b529f8c416a913
akash <akash@sharefable.com>
2022-12-07 18:32:46 +0000

    Delete project dependency

--- schema/V2__Meta_Field_In_AssetProxy.sql
+++
@@ -1,4 +0,0 @@
-USE fable_tour_app;
-
-ALTER TABLE asset_proxy
-    ADD COLUMN meta JSON;
--- src/main/java/com/sharefable/api/repo/ProjectRepo.java
+++
@@ -1,9 +0,0 @@
-package com.sharefable.api.repo;
-
-import com.sharefable.api.entity.Project;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepo extends PagingAndSortingRepository<Project, Long> {
-}
