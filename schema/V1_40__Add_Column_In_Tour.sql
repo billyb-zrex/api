5987ae6cea1523371db7d22bf041c8ae1fff79ea
Fahumitha <125856740+Fahumitha@users.noreply.github.com>
2024-07-10 16:38:05 +0530

    fixing schema (#332)

--- schema/V1_40__Add_Column_In_Tour.sql
+++ schema/V1_40__Add_Column_In_Tour.sql
@@ -1,5 +1,5 @@
 ALTER TABLE fable_tour_app.tour
-    ADD entity_type DEFAULT 0 INTEGER NOT NULL,
+    ADD entity_type INTEGER DEFAULT 0 NOT NULL,
     ADD info JSON NULL;
 
 CREATE INDEX IDX_org_deleted_entity ON fable_tour_app.tour (belongs_to_org, entity_type, deleted);
